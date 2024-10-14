7f235fc4e2b517b65b80f702daf2f6b23994d148
ItsMeSaniya <saninduofficial123@gmail.com>
2024-10-15 02:23:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <Router>
-      <Routes basename="/CRM">
+      <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<LoginRegister />} />
       </Routes>
